f0c7340b751e767b1d841a1094af0d1d8312bcb9
Adam KHNAGUI <khnagui.adam@gmail.com>
2026-04-20 21:43:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header/>
+  <main>
+    <Hero/>
+  </main>
+</template>
+
+<script setup>
+import Hero from "@/components/Hero.vue";
+import Header from "@/components/Header.vue";
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+  <div></div>
+    <nav>
+      <a v-for="(item,index) in buttons" :key="index" :href="item.href">{{item.name}}</a>
+    </nav>
+    <div></div>
+  </header>
+</template>
+
+<script setup>
+
+const buttons = [
+  {href:"#presentation",name:"presentation"},
+  {href:"#projets",name:"projets"},
+  {href:"#competences",name:"competences"},
+  {href:"#contact",name:"contact"},
+]
+
+</script>
+
+<style scoped>
+
+</style>
