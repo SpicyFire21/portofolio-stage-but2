b4348073f4b2f4dc9d52c96f8db5b1561a2da02d
Adam KHNAGUI <khnagui.adam@gmail.com>
2026-04-21 10:32:01 +0200

    features: hero améliorer

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,12 +1,13 @@
 <template>
-  <header>
-    <nav>
+  <header class="flex justify-center">
+    <nav class="flex gap-8">
       <a
           v-for="(item,index) in buttons"
           :key="index"
           :href="item.href"
+          class="relative text-[20px] no-underline transition-all duration-500 hover:text-[var(--pink)] hover:opacity-85 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-[var(--pink)] after:transition-all after:duration-500 hover:after:w-full"
       >
-        {{ item.name }}
+        <p class="transition-all duration-1200 hover:text-[var(--pink)]">{{ item.name }}</p>
       </a>
     </nav>
   </header>
@@ -20,39 +21,3 @@ const buttons = [
   {href:"#contact",name:"contact"},
 ]
 </script>
-
-<style scoped>
-nav {
-  display: flex;
-  gap: 30px;
-}
-
-a {
-  position: relative;
-  font-size: 20px;
-
-  text-decoration: none;
-  transition: color 0.5s ease, opacity 0.5s ease;
-}
-
-/* ligne animée */
-a::after {
-  content: "";
-  position: absolute;
-  left: 0;
-  bottom: -4px;
-  width: 0%;
-  height: 2px;
-  background: var(--pink);
-  transition: width 0.5s ease;
-}
-
-a:hover {
-  color: var(--pink);
-  opacity: 0.85;
-}
-
-a:hover::after {
-  width: 100%;
-}
-</style>
--- src/components/layout/Hero.vue
+++ src/components/layout/Hero.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
-    <h1>Adam KHNAGUI</h1>
-    <h6>Étudiant en 2<span>ème</span> de Bachelor Universitaire et Technologique</h6>
-  </div>
+  <section class="grid grid-cols-12 grid-rows-6 h-[80vh] w-full text-center items-center">
+
+    <h1 class="col-span-10 row-start-3 text-5xl font-bold">
+      Adam KHNAGUI
+    </h1>
+
+    <h2 class="col-span-12 row-start-4 col-start-4 text-lg font-normal">
+      Étudiant en 2<span class="text-[10px] align-top ml-1">ème</span> de Bachelor Universitaire et Technologique
+    </h2>
+
+  </section>
 </template>
 
 <script setup>
-
 </script>
-
-<style scoped>
-span {
-  font-size: 6px;
-  margin-bottom: 15px;
-}
-</style>
